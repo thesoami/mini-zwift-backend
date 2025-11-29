367befa37360043ac780e5de4632a1dd7e30e198
thesoami <scjm007@gmail.com>
2025-11-29 04:09:40 -0500

    first commit/ project setup

---
+++ src/main/java/com/soami/mini_zwift_backend/MiniZwiftBackendApplication.java
@@ -0,0 +1,13 @@
+package com.soami.mini_zwift_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniZwiftBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniZwiftBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soami/mini_zwift_backend/MiniZwiftBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soami.mini_zwift_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniZwiftBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
