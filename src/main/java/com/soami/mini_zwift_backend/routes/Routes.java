8ea8228f89ae7eed62abadfe7a5db63f1c007f30
thesoami <scjm007@gmail.com>
2025-12-01 01:54:21 -0500

    model

---
+++ src/main/java/com/soami/mini_zwift_backend/routes/Routes.java
@@ -0,0 +1,20 @@
+package com.soami.mini_zwift_backend.routes;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Routes {
+    private UUID id;
+    private String name;
+    private Integer distanceKilometers;
+    private Instant createdAt;
+}
